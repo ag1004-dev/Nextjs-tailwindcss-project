33e71a1b76a4bcee3dff9295901ce359d3b9a758
WilliamBennett516 <williambennett116ok@proton.me>
2021-04-22 08:39:08 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index() {
           <div className='w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4'>
             <div className='pt-32 sm:pt-0'>
               <h2 className='font-semibold text-4xl text-blueGray-600'>
-                Over 40 Web Club - A beautiful extension for Tailwind CSS.
+                Over 40 Web Club✨ - A beautiful extension for Tailwind CSS.
               </h2>
               <p className='mt-4 text-lg leading-relaxed text-blueGray-500'>
                 Notus NextJS is Free and Open Source. It does not change any of
